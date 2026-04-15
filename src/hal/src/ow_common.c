e29df8862783c5ce162315aae6bf389eff3f9cc6
Saiteja Dasari <dsaiteja0910@gmail.com>
2026-04-15 15:00:29 -0700

    Add missing ow_common.c with owCommonInit/owCommonTest implementations

---
+++ src/hal/src/ow_common.c
@@ -0,0 +1,37 @@
+/**
+ *    ||          ____  _ __
+ * +------+      / __ )(_) /_______________ _____  ___
+ * | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+ * +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+ *  ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
+ *
+ * Crazyflie control firmware
+ *
+ * Copyright (C) 2011-2012 Bitcraze AB
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ow_common.c - One-wire common functions shared by all OW implementations
+ */
+#include <stdbool.h>
+#include "ow.h"
+
+void owCommonInit()
+{
+  // Common initialization for one-wire subsystem
+}
+
+bool owCommonTest()
+{
+  return true;
+}
